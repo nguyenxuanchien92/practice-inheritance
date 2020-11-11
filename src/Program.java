25d7717ca215dec1e7333c7a2b110ee52eaa672c
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-11 16:22:13 +0700

    done

--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,10 @@
 import models.Circle;
 import models.Cylinder;
 
-public class Main {
+public class Program {
     public static void main(String[] args) {
-
-        Circle circle = new Circle(1,"red");
-        Cylinder cylinder = new Cylinder(1.2,"yellow",20);
+        Circle circle = new Circle(1, "red");
+        Cylinder cylinder = new Cylinder(1.2, "yellow", 29);
 
         System.out.println(circle.toString());
         System.out.println(cylinder.toString());
