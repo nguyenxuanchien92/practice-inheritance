8859412e5c1ba27189154aece7b7a532bbac83ff
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-11 16:06:23 +0700

    create a class Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import models.Circle;
+import models.Cylinder;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(1,"red");
+        Cylinder cylinder = new Cylinder(1.2,"yellow",20);
+
+        System.out.println(circle.toString());
+        System.out.println(cylinder.toString());
+    }
 }
